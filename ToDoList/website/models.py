807aaacda0b970db0b900ff03ee3532d65a37326
Jakub <jakubkmn12@gmail.com>
2023-06-21 16:57:06 +0200

    Creating models

--- ToDoList/website/models.py
+++ ToDoList/website/models.py
@@ -1,3 +1,23 @@
+from django.utils import timezone
 from django.db import models
 
+
+def one_week():
+    return timezone.now() + timezone.timedelta(days=7)
+
 # Create your models here.
+class ToDoList(models.Model):
+    title = models.CharField(max_length=150, unique=True)
+
+    def __str__(self):
+        return str(self.title)
+
+class ToDoItem(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(default=one_week) 
+    todo_list = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title}: due {self.due_date}"
